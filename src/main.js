04fa96abd29bbb5ec91aef7b53e1a216348861f2
Dedridec <59389978+Dedridec@users.noreply.github.com>
2020-12-16 16:06:38 +0100

    Revert "Log page working"

--- src/main.js
+++ src/main.js
@@ -6,33 +6,14 @@ import Menu from "./Menu";
 import CreerSondage from "./components/CreerSondage";
 import ListSondage from "./components/ListSondage";
 import vuetify from '@/plugins/vuetify';
-import Welcome from "./components/Welcome";
-import store from './store';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import VeeValidate from 'vee-validate';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import {
-  faHome,
-  faUser,
-  faUserPlus,
-  faSignInAlt,
-  faSignOutAlt
-} from '@fortawesome/free-solid-svg-icons';
-
-library.add(faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt);
-
-Vue.config.productionTip = false;
-
-Vue.use(VeeValidate);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
+import Welcome from "./components/Welcome"; // path to vuetify export
+//import colors from 'vuetify/lib/util/colors'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter);
 
 const routes = [
-  {path: '/Login', component: Login},
+  {path: '/', component: Login},
   {path: '/Menu', component: Menu,
     children: [
       // `UserHome` va être rendu à l'intérieur du `<router-view>` de `User`
@@ -53,6 +34,5 @@ new Vue({
   el: '#app',
   render: h => h(App),
   vuetify,
-  store,
-  router
+  router,
 });
